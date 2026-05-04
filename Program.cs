05699ac03798443ba0015388d2b889b5b76afa0b
radvanyit <radvanyit@310-06>
2026-05-04 15:32:24 +0200

    utolso feladat

--- Program.cs
+++ Program.cs
@@ -18,17 +18,36 @@
             {
                 összeg += item.Ár;
             }
-            double átlag = összeg/(double)list.Count;
+            double átlag = összeg / (double)list.Count;
             Console.WriteLine("8.feladat\r\nA fürdőbelépők átlagos ára: {átlag:F1}\r\n");
 
-            furdo min= list[0];
+            furdo min = list[0];
             foreach (var item in list)
-               {
+            {
                 if (item.Vízhőfok < min.Vízhőfok)
                     min = item;
             }
             Console.WriteLine($"9. feladat");
             Console.WriteLine($"A leghidegebb víz a(z) {min.Név} nevű fürdőben van.\r\n");
+
+            Console.WriteLine($"10. feladat\r\nKérem, adja  meg a fürdő nevét!");
+            string nev = Console.ReadLine();
+            furdo valasz = null;
+            foreach (var item in list)
+            {
+                if (item.Név == nev)
+                {
+                    valasz = item;
+                }
+            }
+
+            if (valasz != null)
+                Console.WriteLine($"A {valasz.Telepules()} településen van, " +
+                    $"az irányítószám: " +
+                    $"{valasz.IRSZ()}\r\n");
+            else Console.WriteLine("Nincs ilyen nevű fürdő!");
+        }
+
         }
     }
 }
